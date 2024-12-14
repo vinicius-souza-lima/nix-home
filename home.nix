023a36077c6ea7e273f9ec1e5ea4b428d1b81274
Vinícius de Souza Lima <eng.viniciusdesouzalima@gmail.com>
2024-12-14 14:38:35 -0300

    ffmpeg and yt-dlp

--- home.nix
+++ home.nix
@@ -23,6 +23,8 @@
     # pkgs.hello
     pkgs.uv
     pkgs.rustup
+    pkgs.ffmpeg
+    pkgs.yt-dlp
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
